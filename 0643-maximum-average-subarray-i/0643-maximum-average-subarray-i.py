2c3f6911dd97b267a7ae42069a2ced9f924be50f
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-09-28 14:55:10 +0200

    Time: 1914 ms (15.58%), Space: 20.8 MB (98.42%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        left = 0
+        right = left + k
+        current = sum(nums[left:right])
+        max_sum = current
+        while right < len(nums):
+            current = current - nums[left] + nums[right]
+            max_sum = max(max_sum, current)
+            print('max_sum', max_sum)
+            left += 1
+            right += 1
+
+        return max_sum / float(k)
