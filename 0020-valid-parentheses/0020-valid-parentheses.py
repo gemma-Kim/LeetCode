43d74c974dac69bb2df43b453119eae9ed650b6d
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-09-30 12:51:38 +0200

    Time: 11 ms (83.47%), Space: 11.8 MB (47.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -11,7 +11,11 @@ class Solution(object):
             if not stack:
                 stack += ss
             else:
-                if (ss == closes[0] and stack[-1] == opens[0]) or (ss == closes[1] and stack[-1] == opens[1]) or (ss == closes[2] and stack[-1] == opens[2]):
+                if ss == closes[0] and stack[-1] == opens[0]:
+                    stack = stack[:-1]
+                elif ss == closes[1] and stack[-1] == opens[1]:
+                    stack = stack[:-1]
+                elif ss == closes[2] and stack[-1] == opens[2]:
                     stack = stack[:-1]
                 else:
                     stack += ss
