26201bba073ba292f4b5209716e223fa41c45363
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-09-30 12:27:48 +0200

    Time: 63 ms (30.47%), Space: 11.7 MB (91.92%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -7,19 +7,3 @@ class Solution(object):
             if magazine_count[char] < count:
                 return False
         return True
-
-        # m = {}
-        # for i in range(len(magazine)):
-        #     if magazine[i] in m:
-        #         m[magazine[i]] = m[magazine[i]] + 1
-        #     else:
-        #         m[magazine[i]] = 1
-        # for i in range(len(ransomNote)):
-        #     if ransomNote[i] in m:
-        #         if m[ransomNote[i]] > 0:
-        #             m[ransomNote[i]] = m[ransomNote[i]] - 1
-        #         else:
-        #             return False
-        #     else:
-        #         return False
-        # return True
