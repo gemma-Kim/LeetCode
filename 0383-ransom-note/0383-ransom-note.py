2f7b49f6563a51426a32cc56bd2d5c14ce6a766c
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-09-30 12:27:17 +0200

    Time: 68 ms (23.52%), Space: 11.8 MB (71.83%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,17 +1,25 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        m = {}
-        for i in range(len(magazine)):
-            if magazine[i] in m:
-                m[magazine[i]] = m[magazine[i]] + 1
-            else:
-                m[magazine[i]] = 1
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in m:
-                if m[ransomNote[i]] > 0:
-                    m[ransomNote[i]] = m[ransomNote[i]] - 1
-                else:
-                    return False
-            else:
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+        
+        for char, count in ransom_count.items():
+            if magazine_count[char] < count:
                 return False
         return True
+
+        # m = {}
+        # for i in range(len(magazine)):
+        #     if magazine[i] in m:
+        #         m[magazine[i]] = m[magazine[i]] + 1
+        #     else:
+        #         m[magazine[i]] = 1
+        # for i in range(len(ransomNote)):
+        #     if ransomNote[i] in m:
+        #         if m[ransomNote[i]] > 0:
+        #             m[ransomNote[i]] = m[ransomNote[i]] - 1
+        #         else:
+        #             return False
+        #     else:
+        #         return False
+        # return True
