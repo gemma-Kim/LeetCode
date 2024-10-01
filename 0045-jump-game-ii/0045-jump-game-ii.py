e242b512e9aa175ea0e93648de471407665bfe07
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-10-01 14:20:28 +0200

    Time: 81 ms (88.91%), Space: 12.3 MB (97.2%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        position = 0
+        count = 0
+        if len(nums) == 1:
+            return count
+        
+        while position < len(nums):
+            count += 1
+            max_reach = position + nums[position]
+            if max_reach >= len(nums) - 1:                
+                break
+            max_v = 0
+            new_p = position
+            for j in range(position + 1, position + nums[position] + 1):
+                if j < len(nums):    
+                    if max_v <= nums[j] + j:
+                        max_v = nums[j] + j
+                        new_p = j
+                else:
+                    break
+            position = new_p
+        return count
