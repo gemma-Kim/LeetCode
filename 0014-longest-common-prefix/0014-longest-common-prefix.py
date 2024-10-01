3adbe92d2a02253b3ab884488aa77d6e69e33b1d
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-10-01 15:36:47 +0200

    Time: 14 ms (79.23%), Space: 11.6 MB (97.07%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        sorted_strs = sorted(strs, key=len)
+        result = sorted_strs[0]
+        for word in sorted_strs[1:]:
+            if not result:
+                return ""
+            if result == word:
+                continue
+            
+            while result and word and result != word:
+                if len(result) >= len(word):
+                    result = result[:-1]
+                else:
+                    word = word[:len(result)]
+        return result
