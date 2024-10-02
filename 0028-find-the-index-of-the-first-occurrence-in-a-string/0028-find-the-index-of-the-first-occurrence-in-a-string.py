b7e23d94ca27d247b396cd8ddb56e765e1f4f211
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-10-02 11:07:00 +0200

    Time: 13 ms (60.52%), Space: 11.5 MB (84.14%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: int
         """
         # word = ""
-        for i in range(len(haystack)):
+        for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i+len(needle)] == needle:
                 return i
         return -1
