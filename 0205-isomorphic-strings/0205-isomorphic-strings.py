dfa86c2074bbd32ffad49874964765ac372f1ded
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-10-03 11:01:00 +0200

    Time: 88 ms (8.47%), Space: 12.7 MB (82.23%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -6,15 +6,11 @@ class Solution(object):
         :rtype: bool
         """
         tw = {}
-        # tw_keys = []
-        # tw_values = []
         for i in range(len(s)):
             if s[i] in tw.keys() and t[i] in tw.values():
                 if tw[s[i]] != t[i]:
                     return False
-            elif s[i] not in tw.keys() and t[i] not in tw.values():
-                # tw_keys.append(s[i])
-                # tw_values.append(t[i])
+            elif s[i] not in tw.keys() and t[i] not in tw.values():                
                 tw[s[i]] = t[i]
             else:
                 return False
