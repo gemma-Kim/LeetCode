ef8e16eed3e8a77f63a843f29176b6b5cdfed23b
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-10-08 15:48:38 +0200

    Time: 38 ms (86.55%), Space: 11.9 MB (9.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,12 +12,12 @@ class Solution(object):
         """
         if not l1 and not l2:
             return None
+        if not l1 and l2:
+            l1 = ListNode(0)
+        elif l1 and not l2:
+            l2 = ListNode(0)
 
-        current_val = 0
-        if l1:
-            current_val += l1.val
-        if l2:
-            current_val += l2.val
+        current_val = l1.val + l2.val
         next_val = 1 if current_val >= 10 else 0
         if current_val >= 10:
             current_val -= 10
@@ -28,18 +28,12 @@ class Solution(object):
             
         current_node = ListNode(current_val)
         
-        if l1.next and l2.next:              
-            next_node = self.addTwoNumbers(l1.next, l2.next)
-            current_node.next = next_node
-        elif l1.next and not l2.next:              
-            next_node = self.addTwoNumbers(l1.next,ListNode(0))
-            current_node.next = next_node
-        elif not l1.next and l2.next:              
-            next_node = self.addTwoNumbers(ListNode(0), l2.next)
-            current_node.next = next_node
-        else:
+        if not l1.next and not l2.next:
             if next_val == 1:
                 current_node.next = ListNode(1)
-            
+        else:
+            next_node = self.addTwoNumbers(l1.next, l2.next)
+            current_node.next = next_node
+        
         return current_node
         
