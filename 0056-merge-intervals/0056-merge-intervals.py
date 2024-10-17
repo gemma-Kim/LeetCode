feb952354dad74cd3f146b2cbd008632c93765e8
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-10-17 15:42:25 +0200

    Time: 12 ms (100%), Space: 15.3 MB (100%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,22 +5,16 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         intervals = sorted(intervals, key=lambda x: x[0])
-        # print(intervals)
-        # print(intervals)
         result = []
         for interval in intervals:
-            print('interval', interval)
             if result:
                 if result[-1][1] >= interval[0]:
                     start = result[-1][0]
                     end = interval[1] if result[-1][1] < interval[1] else result[-1][1]
-                    print(result)
                     result.pop()
-                    print(result)
                     result.append([start, end])
-                    print(result)
                 else:
-                    result.append(interval)   
+                    result.append(interval) 
             else:
                 result.append(interval)
         return result
