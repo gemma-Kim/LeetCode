859f7c6e02ddf676ea11db49b52d1596c63225b6
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-10-17 15:20:20 +0200

    Time: 43 ms (18.68%), Space: 11.8 MB (98.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,15 +6,12 @@ class Solution(object):
         :rtype: bool
         """
         c_s = Counter(s)
-        print(c_s)
         for a in t:
-            print(a)
             if a in c_s:
                 if c_s[a] > 0:
                     c_s[a] = c_s[a] - 1
-                else:
-                    return False
+                    if c_s[a] == 0:
+                        del c_s[a]
             else:
                 return False
-
-        return True if len(set(c_s.values())) == 1 and list(set(c_s.values()))[0] == 0 else False
+        return False if c_s else True
