0d6e747b743b9a9dd514d9a90d55e55906caa025
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-11-08 16:16:52 +0100

    Time: 22 ms (5.36%), Space: 48.5 MB (59.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -11,43 +11,21 @@ class Solution {
             }
         }
 
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
         
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1]));
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int num = entry.getKey();
-            int frequency = entry.getValue();
-            
-            // Add element to the heap
-            queue.add(new int[]{num, frequency});
-            
-            // If the heap size exceeds k, remove the least frequent element
-            if (queue.size() > k) {
-                queue.poll();
-            }
+            queue.add(new int[]{entry.getKey(), entry.getValue()});
         }
-        
-        // Step 3: Extract the elements from the heap
         int[] result = new int[k];
         int idx = 0;
-        while (!queue.isEmpty()) {
-            result[idx++] = queue.poll()[0];
+        while (k > 0) {
+            int[] v = queue.poll();
+            System.out.println(v[0]);
+            result[idx] = v[0];
+            idx++;
+            k--;
         }
-        
         return result;
-        // Queue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1]));
-        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-        //     queue.add(new int[]{entry.getKey(), entry.getValue()});
-        // }
-        // int[] result = new int[k];
-        // int idx = 0;
-        // while (k > 0) {
-        //     int[] v = queue.poll();
-        //     System.out.println(v[0]);
-        //     result[idx] = v[0];
-        //     idx++;
-        //     k--;
-        // }
-        // return result;
         
     }
 }
