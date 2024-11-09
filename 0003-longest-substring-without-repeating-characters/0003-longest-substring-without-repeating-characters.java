2cf7ac8b33566b0e1c2dd1b5265a4741e9570f9d
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-11-09 17:00:13 +0100

    Time: 7 ms (39.26%), Space: 45.3 MB (9.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,12 +1,9 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
         // define two point
         int left = 0;
-        int right = 1;
-        int result = 1;
+        int right = 0;
+        int result = 0;
         while (left <= right && right < s.length()) {
             String sliced = s.substring(left, right);
             // without repeating characters
