a72e8909f30643d81249930ae67dc88d22973b10
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-11-13 23:42:43 +0100

    Time: 18 ms (9.78%), Space: 48.7 MB (33.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,38 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap();
-        
-        for (int num : nums) {
-            if (map.containsKey(num)) {
-                int f = map.get(num);
-                map.put(num, f + 1);
+        // sorting nums by increasing
+        Arrays.sort(nums);
+        // add how many counts of each value in nums to priority queue (pq)
+        Queue<ArrayList<Integer>> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b.get(1), a.get(1)));
+        // ArrayList<Integer> = [0: num, 1: counts of nums array]
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(nums[0]);
+        l.add(1);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                l.set(1, l.get(1) + 1); // 빈도수 증가
             } else {
-                map.put(num, 1);
+                pq.add(l); // 현재 숫자와 빈도수 추가
+                l = new ArrayList<>(); // 새 리스트로 초기화
+                l.add(nums[i]);
+                l.add(1);
             }
         }
+        pq.add(l); // 마지막 숫자와 빈도수 추가
 
-        
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1]));
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            queue.add(new int[]{entry.getKey(), entry.getValue()});
-        }
+        // make result int array by while loop with k parameter
         int[] result = new int[k];
         int idx = 0;
+        System.out.println(pq);
         while (k > 0) {
-            int[] v = queue.poll();
-            System.out.println(v[0]);
-            result[idx] = v[0];
+            result[idx] = pq.poll().get(0);
             idx++;
             k--;
         }
+
         return result;
-        
+
     }
 }
