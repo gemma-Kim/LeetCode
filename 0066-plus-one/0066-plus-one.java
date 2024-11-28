63b9a6e2410e992bd7281928171b1c3842939e99
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-11-28 10:13:56 +0100

    Time: 0 ms (100%), Space: 42.1 MB (5.5%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         digits[digits.length-1] += 1;
-        System.out.println(digits[0]);
+        
         for (int i = digits.length-1; i > 0; i--) {
             if (digits[i] > 9) {
                 digits[i-1] += 1;
