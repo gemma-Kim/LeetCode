8ccf1618da151f7ddb5613279e4ac30db211807c
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-12-01 10:41:19 +0100

    Time: 18 ms (68.96%), Space: 71.2 MB (33.61%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -0,0 +1,11 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    const map = new Map<number, number>();
+    for (let i = 0; i < nums.length; i++) {
+        const prevIndex = map.get(nums[i]);
+        if (prevIndex !== undefined && i - prevIndex <= k) {
+            return true;
+        }
+        map.set(nums[i], i);
+    }
+    return false;
+};
