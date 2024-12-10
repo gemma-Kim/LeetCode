055c1e19b7bd1fa2eb8aede76f5f122fe64ea8e1
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-12-10 14:01:02 +0100

    Time: 10 ms (80.99%), Space: 43.5 MB (28.67%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.kt
@@ -0,0 +1,29 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun averageOfLevels(root: TreeNode?): DoubleArray {
+        val result = ArrayList<Double>()
+        val currentNodes = LinkedList<TreeNode>()
+        if (root != null) currentNodes.add(root)
+        while (!currentNodes.isEmpty()) {
+            val size = currentNodes.size
+            var sum = 0L
+            for (i in 0..size-1) {
+                val node = currentNodes.poll()
+                if (node.left != null) currentNodes.add(node.left)
+                if (node.right != null) currentNodes.add(node.right)
+                sum += node.`val`
+            }
+            result.add(sum.toDouble() / size)
+        }
+        return result.toDoubleArray()
+    }
+}
