4955b72c6c302c45c3aece934fefc860b98849ad
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-12-12 11:36:56 +0100

    Time: 0 ms (100%), Space: 36.2 MB (17.23%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var idx = 0;
+        for (i in 0 until nums.size) {
+            if (nums[i] != `val`) {
+                nums[idx] = nums[i]
+                idx++
+            }
+        }
+        return idx
+    }
+}
