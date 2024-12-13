bc8f06df349bede1c3a1fc9096cca5bbd2347935
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-12-13 13:50:01 +0100

    Time: 1 ms (19.9%), Space: 44.5 MB (58.72%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,6 +1,5 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length < 2) return nums.length;
         int result = 2;
         int[] copy = Arrays.copyOf(nums, nums.length);
         for (int i = 2; i < nums.length; i++) {
