eb26a326947305bc137a5519926ee8ab45af2eda
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2024-12-17 13:55:10 +0100

    Time: 1 ms (72.91%), Space: 40.8 MB (51.52%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -4,15 +4,11 @@ class Solution {
         while (n != 1) {
             int sum = 0;
             int current = n;
-            // for (String num : String.valueOf(n).split("")) {
-            //     sum += Integer.valueOf(num) * Integer.valueOf(num);
-            // }
             while (current > 0) {
                 int digit = current % 10;
                 sum += digit * digit;
                 current /= 10;
             }
-
             if (set.contains(sum)) return false;
             n = sum;    
             set.add(sum);    
