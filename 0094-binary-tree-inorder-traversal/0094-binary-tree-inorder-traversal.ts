ba1d398d7f010da0ab02050d328aae248981ccd1
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2025-05-12 16:48:26 +0900

    Time: 0 ms (100%), Space: 56.1 MB (21.28%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return [];
+    const result = [];
+
+    function bfs(node: TreeNode | null) {
+        if (node) {
+            if (node.left) bfs(node.left);
+            if (node.val !== null) result.push(node.val);
+            if (node.right) bfs(node.right);   
+        }
+    }
+    bfs(root);
+    return result;
+};
