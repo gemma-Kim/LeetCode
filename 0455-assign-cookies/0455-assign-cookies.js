e850ae3d5e15960c0b432e7a8ef36a557dae051b
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2025-05-28 17:26:37 +0900

    Time: 1324 ms (5.11%), Space: 82.3 MB (5.63%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -5,14 +5,14 @@
  */
 var findContentChildren = function(g, s) {
     let satisfiedCnt = 0;
-    g.sort((a,b) => a-b); // 자식
-    s.sort((a,b) => a-b); // 쿠키
+    g.sort((a,b) => a-b);
+    s.sort((a,b) => a-b);
     while (s.length) {
         if (s[0] >= g[0]) {
             satisfiedCnt++;    
-            g.shift();
+            g = g.slice(1);
         }
-        s.shift();
+        s = s.slice(1);
     }
     return satisfiedCnt;
 };
