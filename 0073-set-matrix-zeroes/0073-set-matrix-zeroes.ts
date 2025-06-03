82e770000542de837dc0a0c12ae4432b9ea41eac
Minseo Kim <54109474+gemma-Kim@users.noreply.github.com>
2025-06-03 16:01:46 +0900

    Time: 20 ms (5.01%), Space: 62.1 MB (7.12%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -0,0 +1,25 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+    const checkerM = new Map();
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix[0].length; c++) {
+            if (matrix[r][c] === 0) {
+                checkerM.set('row' + r, true);
+                checkerM.set('cloumn' + c, true);
+            }
+        }
+    }
+    console.log('checkerM: ', checkerM)
+
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix[0].length; c++) {
+            if (matrix[r][c] !== 0) {
+                if (checkerM.get('row' + r) || checkerM.get('cloumn' + c)) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+};
